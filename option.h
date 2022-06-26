309575af8cf958012deffcf4466bd6f81354822e
CoderTypist <cjbargraser@gmail.com>
2022-06-26 01:45:17 -0500

    Changed Some macro

--- option.h
+++ option.h
@@ -9,7 +9,7 @@ bool is_some(Option_t pOption);
 bool is_none(Option_t pOption);
 
 #define Option(...) Option_t
-#define Some(p) ((void*)p)
+#define Some(p) ((Option_t)p)
 #define None NULL
 
 #endif // _OPTION_H_
