ac746523fe8e5fefbe1b26038af0b70f28c8db74
CoderTypist <cjbargraser@gmail.com>
2022-06-26 01:40:15 -0500

    typo

--- assert.c
+++ assert.c
@@ -3,7 +3,7 @@
 void assert_malloc(void* pVoid, const char* strFunction) {
     if( NULL == pVoid ) {
         if( NULL == strFunction )
-            fprintf(stderr, "Error: malloc failed\n", strFunction);
+            fprintf(stderr, "Error: malloc failed\n");
         else
             fprintf(stderr, "Error: %s(): malloc failed\n", strFunction);
         exit(-1);
